9ed8c558d293a7947ba67d25b9c58a8973790cb2
QuaTrixXx205 <quang2052003@gmail.com>
2024-08-30 21:40:03 +0700

    delete few files :/

--- obj/Debug/net8.0/JWT_Demo.AssemblyInfo.cs
+++ obj/Debug/net8.0/JWT_Demo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JWT_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b716e3dd469995ceb06a713d60e602c5807a487")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd6ff493d258dd1839564ccb7c317e62208ef42")]
 [assembly: System.Reflection.AssemblyProductAttribute("JWT_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JWT_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
