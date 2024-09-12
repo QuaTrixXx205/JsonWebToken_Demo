6e009bd893866bc57498ba5699c796d69220ae44
QuaTrixXx205 <quang2052003@gmail.com>
2024-09-12 10:22:31 +0700

    add resources

--- obj/Debug/net8.0/JWT_Demo.AssemblyInfo.cs
+++ obj/Debug/net8.0/JWT_Demo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JWT_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ed8c558d293a7947ba67d25b9c58a8973790cb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+491506bf212529e1f9c4d1f86899b35a35b65d08")]
 [assembly: System.Reflection.AssemblyProductAttribute("JWT_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JWT_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
